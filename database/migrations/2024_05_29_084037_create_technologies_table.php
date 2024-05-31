766ab22cb396811d4c56a93333217c31a1ccd7f6
Arya Ramesh <150822372+aryadeifiordi@users.noreply.github.com>
2024-05-31 18:14:37 +0200

    adjustments

--- database/migrations/2024_05_29_084037_create_technologies_table.php
+++ database/migrations/2024_05_29_084037_create_technologies_table.php
@@ -4,24 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateTechnologiesTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+            $table->string('name'); // Aggiunto il campo per il nome della tecnologia
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('technologies');
     }
-};
+}
--- database/migrations/2024_05_29_084424_create_project_technology_table.php
+++ database/migrations/2024_05_29_084424_create_project_technology_table.php
@@ -4,24 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProjectTechnologyTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('project_technology', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('project_id'); // Colonna per la chiave esterna del progetto
+            $table->unsignedBigInteger('technology_id'); // Colonna per la chiave esterna della tecnologia
             $table->timestamps();
+
+            // Aggiungi le chiavi esterne
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->foreign('technology_id')->references('id')->on('technologies')->onDelete('cascade');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('project_technology');
     }
-};
+}
