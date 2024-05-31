e1b1878b56c7ee6230fc8c73e8faed0770d7b7fa
Arya Ramesh <150822372+aryadeifiordi@users.noreply.github.com>
2024-05-31 18:30:10 +0200

    technolgy crud

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\Admin\TechnologyController; 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     
     Route::prefix('admin')->name('admin.')->group(function () {
         Route::resource('projects', ProjectController::class);
+        Route::resource('technologies', TechnologyController::class); 
     });
 });
 
